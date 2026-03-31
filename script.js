b16eeb0b3a0d055d3f13389c19a88c1dec7834d5
bristipal279 <bristipal279@gmail.com>
2026-03-31 19:39:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ const appearOnScroll = () => {
 
 window.addEventListener('scroll', appearOnScroll);
 window.addEventListener('load', appearOnScroll);
+
+const toggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+toggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
